644ef78d2a084243c2f30042662af02ce2c1e6ae
Erickkase <ehbonilla@uce.edu.ec>
2026-01-12 14:39:34 -0500

    :wrench: refactor: enhance service change detection logic and improve logging in services

--- services/emotion-service/src/app.ts
+++ services/emotion-service/src/app.ts
@@ -67,6 +67,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   next();
 });
 
+
 // Logger de solicitud
 app.use((req: Request, res: Response, next: NextFunction) => {
   console.log(`[${new Date().toISOString()}] ${req.requestId} ${req.method} ${req.url}`);
